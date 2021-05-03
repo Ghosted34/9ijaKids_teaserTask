d0bf75ddd7e263f9ff5265b482e3d52410d7ba17
Ghosted34 <greyman1934@outlook.com>
2021-05-03 21:10:44 +0100

    hard-coded-data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import {useState, useEffect} from 'react';
-import axios from 'axios';
+import {useState} from 'react';
 import GameCard from './GameCard/GameCard'
 import './App.css';
 
@@ -11,7 +10,7 @@ function App() {
     {"GameTitle":"Kiddiepreneur 101","GameDescription":"Kiddiepreneur 101 (Intro to Earning and Spending)","Topic":"Financial Literacy","Group":"Financial Literacy","Level":"Financial Literacy","Subject":"Financial Literacy","GameImage":"https://partners.9ijakids.com/index.php/thumbnail?game=Kiddiepreneur 101"},
     {"GameTitle":"Money Matters","GameDescription":"Money Matters (Intro to Key Financial Terms)","Topic":"Financial Literacy","Group":"Financial Literacy","Level":"Financial Literacy","Subject":"Financial Literacy","GameImage":"https://partners.9ijakids.com/index.php/thumbnail?game=Money Matters"},
     {"GameTitle":"Maths Pop","GameDescription":"Maths Pop (writing numbers in word, sequencing & ordinal numbers)","Topic":"Number Sense","Group":"Academic","Level":"Key Stage 1","Subject":"Mathematics","GameImage":"https://partners.9ijakids.com/index.php/thumbnail?game=Maths Pop"},
-    {"GameTitle":"Exploring Life","GameDescription":"Exploring Life KS","Topic":"Living Things & Non-Living Things","Group":"Academic","Level":"Key Stage 1","Subject":"Science","GameImage":"https://partners.9ijakids.com/index.php\/thumbnail?game=Exploring Life"},
+    {"GameTitle":"Exploring Life","GameDescription":"Exploring Life KS","Topic":"Living Things & Non-Living Things","Group":"Academic","Level":"Key Stage 1","Subject":"Science","GameImage":"https://partners.9ijakids.com/index.php/thumbnail?game=Exploring Life"},
     {"GameTitle":"Mathsmania City - Decimals","GameDescription":"Mathsmania City - Decimal","Topic":"Decimals, Fractions & Percentage","Group":"Academic","Level":"Key Stage 2","Subject":"Mathematics","GameImage":"https://partners.9ijakids.com/index.php/thumbnail?game=Mathsmania City - Decimals"}]
   );
 
